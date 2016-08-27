6c14af97a548fd3b063aa863e3acae730596bb3d
Jackson Marketon <jmarketon@gmail.com>
2016-08-27 17:55:45 -0500

    progress

--- bin/index.js
+++ bin/index.js
@@ -3,10 +3,10 @@
 
 // Module Dependencies
 const program = require('commander');
-const pjson = require('./../package.json');
 const debug = require('debug')('bin');
-const configParser = require('./../lib/configParser');
+const pjson = require('./../package.json');
 const blessedEngine = require('./../lib/blessedEngine');
+const path = require('path');
 
 let config = {};
 
@@ -17,42 +17,23 @@ program
     '-c, --config [path]',
     'Config file, no default expects json file',
     (val) => val,
-    false
+    'multidash.config.js'
   )
   .option(
-    '-p, --process [value]',
-    'Commands to spawn (curr limit is 2',
-    (curr, acc) => acc.concat(curr),
-    []
+    '-m, --mode [value]',
+    'Mode to run multidash in',
+    (val) => val,
+    'dev'
   )
-  // This will change to true once @TODO for useable terminals is implimented
-  .option('-t, --terminal', 'Spawn a usable terminal')
   .parse(process.argv);
-
-program.process = program.process;
-program.debug = program.debug || 0;
-program.terminal = true === program.terminal;
 
 debug(`
 - Config (file path): ${program.config}
-- Processes (bash): "${program.process.join('" && "')}"
-- Terminal (boolean): ${program.terminal}
 `);
 
-// if no config or less than two proc commands die
-if (!program.config && 0 === program.process.length) {
-  throw new Error('Need a config file [-c] OR processes to run [-p]');
-} else if (!program.config && 2 > program.process.length) {
-  throw new Error('Need two or more processes to run.');
-}
+config = require(path.resolve(process.cwd(), program.config));
+debug(`
+- Config (parsed): ${config}
+`);
 
-config = program.opts();
-
-// @TODO this isn't working atm as I hate dealing with async file reads
-// if (program.config) {
-//   debug(`Using config file: ${program.config}`);
-//   const parsed = configParser(program.config);
-//   config = Object.assign(parsed, config);
-// }
-
-blessedEngine(config);
+blessedEngine(config, process.mode);
